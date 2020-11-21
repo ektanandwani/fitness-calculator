c2cf6d7dc0b8a7be1cb04cca7865feb2d5a9719f
Ekta Nandwani <Ekta.Nandwani.iiitb.org>
2020-11-21 17:26:25 +0530

    Initial Commit

---
+++ src/main/java/com/example/calculator/BmiCalculator.java
@@ -0,0 +1,37 @@
+package com.example.calculator;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class BmiCalculator {
+    private static String calculateBmi(int height, double weight) {
+        final double countBmi = (weight / Math.pow(height, 2)) * 10000;
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(1);
+        df.setMinimumFractionDigits(1);
+
+        String outputText = "\nYour BMI is: " + df.format(countBmi);
+
+        if (countBmi < 18.5) {
+            return "Underweight" + outputText;
+        } else if (countBmi > 18.5 && countBmi < 24.9) {
+            return "Healthy" + outputText;
+        } else if (countBmi > 25 && countBmi < 30) {
+            return "Overweight" + outputText;
+        } else if (countBmi > 30) {
+            return "Obese" + outputText;
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your height: (in meters)");
+        int setHeight = scanner.nextInt();
+        System.out.println("Enter your weight: (in kilograms)");
+        double setWeight = scanner.nextDouble();
+
+        System.out.println(calculateBmi(setHeight, setWeight));
+    }
+}
