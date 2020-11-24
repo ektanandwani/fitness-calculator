19082527b5b91c80f97a5cabc0ddf21537f91b5c
smitlimbani <slsrlsmith91@gmail.com>
2020-11-25 01:27:35 +0530

    Added BmiCalculator tests.

---
+++ src/test/java/com/example/converter/HeightConverterTest.java
@@ -0,0 +1,32 @@
+package com.example.converter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HeightConverterTest {
+
+    @Test
+    public void cmToFoot() {
+    }
+
+    @Test
+    public void cmTometer() {
+    }
+
+    @Test
+    public void meterToFoot() {
+    }
+
+    @Test
+    public void meterToCm() {
+    }
+
+    @Test
+    public void footToCm() {
+    }
+
+    @Test
+    public void footToMeter() {
+    }
+}
---
+++ src/test/java/com/example/converter/MassConverterTest.java
@@ -0,0 +1,32 @@
+package com.example.converter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MassConverterTest {
+
+    @Test
+    public void kgToGram() {
+    }
+
+    @Test
+    public void kgToPound() {
+    }
+
+    @Test
+    public void gramToKg() {
+    }
+
+    @Test
+    public void gramToPound() {
+    }
+
+    @Test
+    public void poundToGram() {
+    }
+
+    @Test
+    public void poundToKg() {
+    }
+}
