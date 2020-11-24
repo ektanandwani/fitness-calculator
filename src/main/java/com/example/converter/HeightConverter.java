ce6e47231510700aca269923ca08a66287b19c0a
Ekta Nandwani <Ekta.Nandwani.iiitb.org>
2020-11-24 21:03:13 +0530

    height converter

--- src/main/java/com/example/converter/HeightConverter.java
+++ src/main/java/com/example/converter/HeightConverter.java
@@ -1,5 +1,29 @@
 package com.example.converter;
 
 public class HeightConverter {
-	// metre, cm to feet, inches and vice versa
+	// meter, cm to feet and vice versa
+	
+	private static double cmToFoot(double cm) {
+		return cm*0.0328084;
+	}
+	
+	private static double cmTometer(double cm) {
+		return cm/100;
+	}
+	
+	private static double meterToFoot(double meter) {
+		return meter*3.28084;
+	}
+	
+	private static double meterToCm(double meter) {
+		return meter*100;
+	}
+	
+	private static double footToCm(double foot) {
+		return foot*30.480000975359999416;
+	}
+	
+	private static double footToMeter(double foot) {
+		return foot*0.3048000097536;
+	}
 }
