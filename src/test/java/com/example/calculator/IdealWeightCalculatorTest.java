6ca46b0f7633230c9118ef1c861c994fd7ccb9a5
Rachna Jain <Rachna.Jain@iiitb.org>
2020-11-25 18:07:53 +0530

    ideal calc test cases

--- src/test/java/com/example/calculator/IdealWeightCalculatorTest.java
+++ src/test/java/com/example/calculator/IdealWeightCalculatorTest.java
@@ -7,10 +7,26 @@ import static org.junit.Assert.*;
 public class IdealWeightCalculatorTest {
 
     @Test
-    public void calcWeight() {
-    }
-
-    @Test
     public void calculate() {
+        IdealWeightCalculator idealWeightCalculator = new IdealWeightCalculator();
+        String res;
+
+        res=idealWeightCalculator.calculate(10,'F',0);
+        assertEquals("Invalid Inputs", res);
+
+        res=idealWeightCalculator.calculate(1,'f',60);
+        assertEquals("Invalid Inputs", res);
+
+        res=idealWeightCalculator.calculate(18,'M',165);
+        assertNotEquals("Invalid Inputs", res);
+
+        res=idealWeightCalculator.calculate(25,'f',175);
+        assertNotEquals("Invalid Inputs", res);
+
+        res=idealWeightCalculator.calculate(85,'m',150);
+        assertEquals("Invalid Inputs", res);
+
+        res=idealWeightCalculator.calculate(30,'F',164);
+        assertNotEquals("Invalid Inputs", res);
     }
 }
